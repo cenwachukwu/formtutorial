2d3660661069af49e9f35f6761c779ab1a545372
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2020-06-08 13:51:07 -0400

    handled validation

--- src/App.js
+++ src/App.js
@@ -12,18 +12,49 @@ function App() {
   const [nameError, setNameError] = useState(null);
   const [emailError, setEmailError] = useState(null);
 
+  // we also need a disable state to prevent the user from clicking the submit button if there is an error
+  const [disabled, setDisabled] = useState(true);
+  // our form validation will check if name or email is empty.
+  // if it is, it will change the state of the error message to what we would want to display
+  // if it's not, it will change the state of the disable to false and our user can submit their forms
+
+  // validEmailRegex
+  const validEmailRegex = RegExp(
+    /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+  );
+
+  const handleformValidation = () => {
+    if (name === '' && email === '') {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const handleFormChange = (event) => {
+    event.preventDefault();
+    const { name, value } = event.target;
+
+    switch (name) {
+      case 'name':
+        setNameError(value.length ? null : 'Name can not be blank!');
+        break;
+      case 'email':
+        setEmailError(validEmailRegex.test(value) ? null : 'Email is not valid!');
+        break;
+
+      default:
+        break;
+    }
+  };
+
   //i know we dont want to run our form validation on first render
   // we want to run it when there are changes to our name and  email state
   // useRef is like a “box” that can hold a mutable value in its .current property.
   // we want to use useRef to set a mutable state or like a toggle inside the useEffect hook
   // so that useEffect doesnt run on the first render
   const isFirstRender = useRef(true);
-
-  // we also need a disable state to prevent the user from clicking the submit button if there is an error
-  const [disabled, setDisabled] = useState(true);
-  // our form validation will check if name or email is empty.
-  // if it is, it will change the state of the error message to what we would want to display
-  // if it's not, it will change the state of the disable to false and our user can submit their forms
+  useEffect(() => {}, [name, email]);
 
   return (
     <div className="App">
@@ -31,11 +62,19 @@ function App() {
         <ul>
           <li>
             <label htmlFor="name">Name</label>
-            <input type="name" name="name" id="name" onChange={(e) => setName(e.target.value)} />
+            <input type="name" name="name" id="name" onChange={handleFormChange} />
+            {nameError ? <span className="error">{nameError}</span> : null}
           </li>
           <li>
             <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="email" onChange={(e) => setEmail(e.target.value)} />
+            <input type="email" name="email" id="email" onChange={handleFormChange} />
+            {emailError ? <span className="error">{emailError}</span> : null}
+          </li>
+          {nameError && <p>{nameError}</p>}
+          <li>
+            <button type="submit" disabled={disabled}>
+              Save
+            </button>
           </li>
         </ul>
       </form>
