01a6819df46e3bd768289b1b8a9c7df95efb4d95
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2020-06-08 13:00:16 -0400

    added the email state and added more order to the div

--- src/App.js
+++ src/App.js
@@ -4,11 +4,22 @@ import './App.scss';
 function App() {
   // name
   const [name, setName] = useState('');
+  // email
+  const [email, setEmail] = useState('');
+
   return (
     <div className="App">
       <form>
-        <label htmlFor="name">Name</label>
-        <input type="text" name="text" id="name" onChange={(e) => setName(e.target.value)} />
+        <ul>
+          <li>
+            <label htmlFor="name">Name</label>
+            <input type="name" name="name" id="name" onChange={(e) => setName(e.target.value)} />
+          </li>
+          <li>
+            <label htmlFor="email">Name</label>
+            <input type="email" name="email" id="email" onChange={(e) => setEmail(e.target.value)} />
+          </li>
+        </ul>
       </form>
     </div>
   );
