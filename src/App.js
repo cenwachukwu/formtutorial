e293abb0e9a71a903684add1b8b8b0ca71d7377f
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2020-06-08 13:21:35 -0400

    logic for the form validation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import './App.scss';
 
 function App() {
@@ -6,6 +6,19 @@ function App() {
   const [name, setName] = useState('');
   // email
   const [email, setEmail] = useState('');
+
+  // we need error messages to display to the user
+
+  //i know we dont want to run our form validation on first render
+  // we want to run it when there are changes to our name and  email state
+  // useRef is like a “box” that can hold a mutable value in its .current property.
+  // we want to use useRef to set a mutable state or like a toggle inside the useEffect hook
+  // so that useEffect doesnt run on the first render
+
+  // we also need a disable state to prevent the user from clicking the submit button if there is an error
+  // our form validation will check if name or email is empty.
+  // if it is, it will change the state of the error message to what we would want to display
+  // if it's not, it will change the state of the disable to false and our user can submit their forms
 
   return (
     <div className="App">
@@ -16,7 +29,7 @@ function App() {
             <input type="name" name="name" id="name" onChange={(e) => setName(e.target.value)} />
           </li>
           <li>
-            <label htmlFor="email">Name</label>
+            <label htmlFor="email">Email</label>
             <input type="email" name="email" id="email" onChange={(e) => setEmail(e.target.value)} />
           </li>
         </ul>
